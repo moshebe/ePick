4947b909fb3848cbafa5f250fa811fd359d6307b
Assaf Shalin <Assaf.Shalin@Gmail.com>
2015-11-13 18:10:13 +0200

    removed

--- controllers/pick.js
+++ controllers/pick.js
@@ -1,7 +1,6 @@
 module.exports = {
 	generatePick: function (req, res) {
 		app.models.Poll.findById(req.params.pickItem, {include: [{all:true}]}).then(function (poll) {
-			console.log(poll.id);
 			res.render('pick', {poll: poll});
 		});
 		
