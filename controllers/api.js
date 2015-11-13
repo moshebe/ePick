b425ef551056c5a710ca012b6fa0ae329ada5ad1
Assaf Shalin <Assaf.Shalin@Gmail.com>
2015-11-13 17:54:44 +0200

    re-fixed conflicts

--- controllers/api.js
+++ controllers/api.js
@@ -2,35 +2,34 @@ var ebay = require('../helpers/ebay.js');
 var Promise = require('bluebird');
 
 module.exports =  {
-    createPick: function (req, res) {
-        if(!req.require(['products', 'question'])) return;
+  createPick: function (req, res) {
+    if(!req.require(['products', 'question'])) return;
 
-        var productsPromises = [];
-      
-        JSON.parse(req.body.products).forEach(function (productId){
-          productsPromises.push(ebay.getProductInfo(productId));
-         });
+    var productsPromises = [];
+    var obj = {};
 
-        Promise.all(productsPromises).then(function (productsInfo){
-          
-          app.models.Poll.create({
-            question: req.body.question
-          })
-          .then(function (poll){
-            res.json(poll);
-            return poll;
-          })
-          .then(function (poll){
-            poll.addProducts(productsInfo);
-          });
+    JSON.parse(req.body.products).forEach(function (productId){
+      productsPromises.push(ebay.getProductInfo(productId));
+    });
 
+    app.models.Poll.create({
+      question: req.body.question
+    })
+    .then(function (poll){
+      res.json({id: poll.id, url: 'http://' + req.headers.host + '/Pick/'+ poll.id + '/'});
+      obj.poll = poll;
+      return Promise.all(productsPromises);
+    })
+    .then(function(productsInfo){
+      for(var key in productsInfo){
+        productsInfo[key].PollId = obj.poll.id;
+      }
+      return app.models.Product.bulkCreate(productsInfo);      
+    });
 
+  },
 
-        });
-       
-    },
- 
-    getProductInfo: function (req, res) {
+  getProductInfo: function (req, res) {
         // if(!req.require(['productIds'])) return;
 
         ebay.getProductInfo('371320064687').then(function (productInfo) {
