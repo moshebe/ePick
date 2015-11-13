551ab1c043eea493dafdc1b155ca0d54cdaa5078
Assaf Shalin <Assaf.Shalin@Gmail.com>
2015-11-13 17:23:02 +0200

    Added template for friend

--- controllers/pick.js
+++ controllers/pick.js
@@ -1,5 +1,34 @@
 module.exports = {
 	generatePick: function (req, res) {
-		res.render('pick')
+		var poll = {
+			id: 37,
+			question: "am I a Cat?",
+			products: [
+			{
+				id: 1,
+				picture: 'http://thecatapi.com/api/images/get?format=src&type=png',
+				url: 'http://ebay.com/api/images/get?format=src&type=png',
+				title: 'Im a Cat'
+			},
+			{
+				id: 2,
+				picture: 'http://thecatapi.com/api/images/get?format=src&type=png',
+				url: 'http://ebay.com/api/images/get?format=src&type=png',
+				title: 'Im a Cat'
+			},
+			{
+				id: 3,
+				picture: 'http://thecatapi.com/api/images/get?format=src&type=png',
+				url: 'http://ebay.com/api/images/get?format=src&type=png',
+				title: 'Im a Cat'
+			},
+			{
+				id: 4,
+				picture: 'http://thecatapi.com/api/images/get?format=src&type=png',
+				url: 'http://ebay.com/api/images/get?format=src&type=png',
+				title: 'Im a Cat'
+			}]
+		};
+		res.render('pick', poll);
 	}
 };
