5328af04f8a7430ec599917f8da5e44fa1e6fe30
Moshe Beladev <moshebe123@gmail.com>
2015-11-13 20:50:54 +0200

    add percents to the statistics

--- controllers/pick.js
+++ controllers/pick.js
@@ -16,13 +16,29 @@ module.exports = {
 	},
 
 	statistics: function(req, res){
-		app.models.Vote.findAll({
-			attributes: ['Vote.ProductId', [app.models.sequelize.fn('count', app.models.sequelize.col('Vote.id')), 'votes']],
-			where: {PollId: req.params.pickId},
-			include: [{model: app.models.Product}],
-			group: ['Vote.ProductId']
+		var obj = {};
+
+		app.models.Vote.count({where: {PollId: req.params.pickId}}).then(function (count){
+			obj.count = count;
+
+			return app.models.Vote.findAll({
+				attributes: ['Vote.ProductId', [app.models.sequelize.fn('count', app.models.sequelize.col('Vote.id')), 'votes']],
+				where: {PollId: req.params.pickId},
+				include: [{model: app.models.Product}],
+				group: ['Vote.ProductId'],
+			});
+
 		}).then(function (results){
+			
+			for (var key in results){
+				results[key] = results[key].get({plain: true});
+	
+				results[key]['percents'] = (results[key].votes / obj.count) * 100;		
+			}
+
 			res.json(results);
 		});
+
+
 	}
 };
