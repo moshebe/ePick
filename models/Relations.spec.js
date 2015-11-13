9a55d287b481267c968d4185c010044aa2d587fd
Assaf Shalin <Assaf.Shalin@Gmail.com>
2015-11-13 14:20:17 +0200

    Updated relations

--- models/Relations.spec.js
+++ models/Relations.spec.js
@@ -1,6 +1,6 @@
 module.exports = function (models) {
 	models.Poll.hasMany(models.Product);
-	models.Poll.hasMany(models.Product);
 	models.Vote.hasOne(models.Product);
 	models.Poll.hasMany(models.Vote);
+	models.Product.belongsTo(models.Poll);
 };
