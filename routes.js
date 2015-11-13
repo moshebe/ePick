c689481b2ef1d32054bed3e11ccdfe582897f323
Moshe Beladev <moshebe123@gmail.com>
2015-11-13 20:53:21 +0200

    change statistics from POST to GET

--- routes.js
+++ routes.js
@@ -2,6 +2,6 @@ app.get('/Pick/:pickItem?', app.controllers.pick.generatePick);
 app.get('/ProductInfo/:productId', app.controllers.api.getProductInfo);
 app.post('/CreatePick', app.controllers.api.createPick);
 app.post('/Pick/:pickId/Vote/:productId', app.controllers.pick.vote);
-app.post('/Pick/:pickId/Statistics', app.controllers.pick.statistics);
+app.get('/Pick/:pickId/Statistics', app.controllers.pick.statistics);
 
 
