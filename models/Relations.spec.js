9b4d84a905c7cfe8302432924eefeed271a72dd0
Moshe Beladev <moshebe123@gmail.com>
2015-11-13 19:21:46 +0200

    Fix relations

--- models/Relations.spec.js
+++ models/Relations.spec.js
@@ -1,6 +1,11 @@
 module.exports = function (models) {
+
 	models.Poll.hasMany(models.Product);
-	models.Vote.hasOne(models.Product);
-	models.Poll.hasMany(models.Vote);
+
 	models.Product.belongsTo(models.Poll);
+	models.Vote.belongsTo(models.Poll);
+
+
+	models.Product.hasMany(models.Vote);
+	models.Vote.belongsTo(models.Product);	
 };
